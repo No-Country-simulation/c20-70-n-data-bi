f27e1a2a7cf368469d21d80ae57663270a0040a4
andresgvelasquez <andresgvelasquez8@gmail.com>
2024-09-13 02:10:12 -0700

    Se añade la función de predicción de fraudes por día.

--- streamlit_app/Home.py
+++ streamlit_app/Home.py
@@ -52,22 +52,50 @@ if codigo_acceso == "1234":
         st.subheader("Estados con Fraude")
         st.dataframe(top_5_fraud_state.set_index(top_5_fraud_state.columns[0]))
     
-    global_frauds_per_day = pd.read_csv('./streamlit_app/data/global_frauds_per_day.csv', index_col='trans_date_trans_time')
+    global_frauds_per_day = pd.read_csv('./streamlit_app/data/global_frauds_per_day.csv', parse_dates=['trans_date_trans_time'], index_col='trans_date_trans_time')
 
     # Crear una figura de Plotly para la gráfica de línea
+
+    # Leer datos de tmp_series_pred
+    tmp_series_pred = pd.read_csv('./streamlit_app/data/tmp_series_pred.csv', parse_dates=['date'], index_col='date')
+
+    # Definir la fecha límite para las predicciones
+    end_date = '2020-12-22'
+
+    # Filtrar las predicciones hasta la fecha límite
+    tmp_series_pred = tmp_series_pred[tmp_series_pred.index <= end_date]
+
+    # Identificar la fecha de inicio de las predicciones
+    start_date_predictions = tmp_series_pred.index.min()
+
+    # Filtrar los datos reales hasta la fecha de inicio de las predicciones
+    global_frauds_per_day = global_frauds_per_day[global_frauds_per_day.index <= start_date_predictions]
+
+
+    # Crear la figura
     fig = go.Figure()
 
-    # Añadir una línea con las fechas y el número de fraudes
+    # Añadir la serie de fraudes originales
     fig.add_trace(go.Scatter(
         x=global_frauds_per_day.index, 
         y=global_frauds_per_day['total_transacciones'], 
         mode='lines', 
-        #name='Número de Fraudes'
+        name='Número de Fraudes',  # Nombre para la leyenda
+        line=dict(color='blue')    # Color para la serie original
+    ))
+
+    # Añadir la serie de predicciones
+    fig.add_trace(go.Scatter(
+        x=tmp_series_pred.index, 
+        y=tmp_series_pred['prediction'],  # Cambia 'prediction' si el nombre de la columna es diferente
+        mode='lines', 
+        name='Predicciones',  # Nombre para la leyenda
+        line=dict(color='red')  # Color para las predicciones
     ))
 
     # Añadir título y etiquetas a los ejes
     fig.update_layout(
-        title="Línea Temporal de Fraudes por día",
+        title="Línea Temporal de Fraudes y Predicciones por Día",
         xaxis_title="Fecha",
         yaxis_title="Cantidad de Fraudes",
         template="plotly_white"
